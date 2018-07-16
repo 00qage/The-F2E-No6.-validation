e456dcd02190a6f9530bb2c0a50c1a3f2ee07397
00qage <z2369364@hotmail.com>
2018-07-16 10:52:22 +0800

    updata

---
+++ js/all.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    //按next時如果沒內容變橘色
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        if ($('#useraccount').val() == '') {
+            $('.useraccount').addClass('error');
+        } else if ($('#userpassword').val() == '') {
+            $('.userpassword').addClass('error');
+        } else if ($('#userpasswordconfirm').val() == '') {
+            $('.userpasswordconfirm').addClass('error');
+        }
+    });
+    //點input時變回原樣
+    $('.input input').focus(function (e) {
+        e.preventDefault();
+        $('.input').removeClass('error');
+        $('.btn').removeClass('allfill');
+    });
+    //監控每個input都有東西時btn才變色
+    $('.form > input').blur(function (e) {
+        e.preventDefault();
+        if ($('#useraccount').val() !== '' && $('#userpassword').val() !== '' && $('#userpasswordconfirm').val() !== '') {
+            $('.btn').addClass('allfill');
+        }
+    });
+
+
+});
